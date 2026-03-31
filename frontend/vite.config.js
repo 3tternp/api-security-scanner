0db7b59062d7ad77d8a9d7ec471e6e24839317be
3tternp <3tternp@users.noreply.github.com>
2026-03-31 12:13:32 +0545

    Fix favicon.ico 404 in dev server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'favicon-no-404',
+      configureServer(server) {
+        server.middlewares.use('/favicon.ico', (req, res) => {
+          res.statusCode = 204
+          res.end()
+        })
+      },
+      configurePreviewServer(server) {
+        server.middlewares.use('/favicon.ico', (req, res) => {
+          res.statusCode = 204
+          res.end()
+        })
+      },
+    },
+  ],
 })
 
